b9da8991cdbf2c10a2d16c805f6745864f180590
Rishabh Yadav <1998rishabh31@gmail.com>
2022-05-07 14:37:30 +0530

    Create Console Functions.js

---
+++ Console Functions.js
@@ -0,0 +1,23 @@
+
+//starts timer
+console.time('Your code took');
+
+//logs string
+console.log("Hello World");
+
+//logs object
+console.log([1,2,3,4,5]);
+
+//logs table
+console.table({rishabh:'this',marks:34});
+console.table({lucky:'that',marks:44});
+
+//logs assertion
+console.assert(5<4,'This condition is false');
+
+//logs warning
+console.warn("This is a warning");
+
+
+//ends timer
+console.timeEnd('Your code took');
